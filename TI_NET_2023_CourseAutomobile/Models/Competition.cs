caddb914306d4546a72c59742757e874e9e1929b
Sebastien <byasebastien@hotmail.com>
2023-06-27 16:22:09 +0200

    bestlap

--- TI_NET_2023_CourseAutomobile/Models/Competition.cs
+++ TI_NET_2023_CourseAutomobile/Models/Competition.cs
@@ -9,14 +9,14 @@ namespace TI_NET_2023_CourseAutomobile.Models
     public class Competition
     {
         public List<Car> Cars { get; set; }
-        public Dictionary<Car, double> Result { get; set; }
+        public Dictionary<Car, List<double>> Result { get; set; }
         public RaceTrack RaceTrack { get; set; }
         public int LapNumber { get; set; }
 
         public Competition(RaceTrack raceTrack, int lapNumber)
         {
             Cars = new List<Car>();
-            Result = new Dictionary<Car, double>();
+            Result = new Dictionary<Car, List<double>>();
             RaceTrack = raceTrack;
             LapNumber = lapNumber;
         }
@@ -30,7 +30,7 @@ namespace TI_NET_2023_CourseAutomobile.Models
         {
             foreach (Car car in Cars)
             {
-                Result.Add(car, 0);
+                Result.Add(car, new List<double>());
             }
         }
 
@@ -46,7 +46,7 @@ namespace TI_NET_2023_CourseAutomobile.Models
                     int second = (int)temps % 60;
                     Console.WriteLine(car);
                     Console.WriteLine($"Effectue le tour {i + 1} avec un temp de {minute.ToString("D2")} : {second.ToString("D2")}");
-                    Result[car] += temps;
+                    Result[car].Add(temps);
                     Console.WriteLine("__________________________________________________");
                 }
                 Console.WriteLine("__________________________________________________");
@@ -56,12 +56,37 @@ namespace TI_NET_2023_CourseAutomobile.Models
 
         public void ShowWinner()
         {
-            KeyValuePair<Car,double> kvpWinner = Result.OrderBy(kvp => kvp.Value).First();
+            //double bestTime = double.MaxValue;
+            //Car winner;
+            //foreach(KeyValuePair<Car, List<double>> kvp in Result)
+            //{
+            //    if(kvp.Value.Sum() < bestTime)
+            //    {
+            //        bestTime = kvp.Value.Sum();
+            //        winner = kvp.Key;
+            //    }
+            //}
+
+            KeyValuePair<Car,List<double>> kvpWinner = Result.OrderBy(kvp => kvp.Value.Sum()).First();
             Console.WriteLine("Le gagnant est : ");
             Console.WriteLine(kvpWinner.Key);
-            int minute = (int)kvpWinner.Value / 60;
-            int second = (int)kvpWinner.Value % 60;
+            double total = kvpWinner.Value.Sum();
+            int minute = (int)total / 60;
+            int second = (int)total % 60;
             Console.WriteLine($"Avec un temp total de {minute.ToString("D2")} : {second.ToString("D2")}");
+            Console.WriteLine("____________________________________________________________________________");
+            KeyValuePair<Car,List<double>> kvpBestLap = Result.OrderBy(kvp => kvp.Value.Min()).First();
+            Console.WriteLine("Le participant qui a fait le meilleur tour est : ");
+            Console.WriteLine(kvpBestLap.Key);
+            double totalBestLap = kvpBestLap.Value.Min();
+            int minuteBestLap = (int)totalBestLap / 60;
+            int secondBestLap = (int)totalBestLap % 60;
+            Console.WriteLine($"Avec un temp de {minuteBestLap.ToString("D2")} : {secondBestLap.ToString("D2")}");
+
+            if(kvpWinner.Key == kvpBestLap.Key)
+            {
+                Console.WriteLine("INCROYABLE!!!");
+            }
         }
     }
 }
